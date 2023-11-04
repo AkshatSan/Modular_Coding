26fdc744b3a6438dc71b075e73e938452e9e6de3
AkshatSan <aaaakshaat@gmail.com>
2023-11-04 19:16:00 +0530

    Data ingestion part has been done

--- src/exception.py
+++ src/exception.py
@@ -22,4 +22,11 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Division by 0")
+        raise CustomException(e,sys)
 
